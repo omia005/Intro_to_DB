6ddb5d68985dc3b75e102309c953c5a0141a9840
Evans Omia <evansomia005@gmail.com>
2025-10-20 01:15:01 +0400

    Create task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,12 @@
+SELECT 
+    COLUMN_NAME, 
+    DATA_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_DEFAULT,
+    COLUMN_KEY,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'books';
